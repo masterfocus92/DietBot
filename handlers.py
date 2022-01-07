7478862ee5e2d81c4b3a37a5ecacdedba8ab02fd
masterfocus92 <masterfocus92@gmail.com>
2022-01-07 23:46:53 +0500

    handlers was refactored

--- handlers.py
+++ handlers.py
@@ -4,51 +4,66 @@ import logging
 
 
 logger = logging.getLogger(__name__)
+START_MESSAGE = ("Привет! Я бот помогающий тебе освоить разумное питание!\n\n"
+                "Я еще очень маленький, и умею только считать нужную тебе для" 
+                "выживания калорийность питания. " 
+                "Введи команду /bmr чтобы я посчитал твой базовый "
+                "уровень метаболизма.")
+
+BMR_MESSAGE =("Давай посчитаем сколько калорий требует " 
+            "твой базовый уровень метаболизма. \n"
+            "Отправь /cancel чтобы прекратить разговор.\n\n"
+            "Ты мальчик или девочка?")
 
 GENDER, AGE, WEIGHT, HEIGHT = range(4)
 
-class UserParams:
+class User:
     def set_gender(self, user_answer: str):
-        if user_answer == "Мальчик": setattr(self, 'is_male', True)
-        else: setattr(self, 'is_male', False)
+        if user_answer == "Мальчик": setattr(self, "is_male", True)
+        else: setattr(self, "is_male", False)
 
-    def set_age(self, str_age: str):
-        age = int(str_age)
-        setattr(self, 'age', age)
+    def set_age(self, user_answer: str):
+        age = int(user_answer)
+        setattr(self, "age", age)
 
-    def set_height(self, str_height: str):
-        height = int(str_height)
-        setattr(self, 'height', height)
+    def set_height(self, user_answer: str):
+        height = int(user_answer)
+        setattr(self, "height", height)
 
-    def set_weight(self, str_weight: str):
-        weight = int(str_weight)
-        setattr(self, 'weight', weight)
+    def set_weight(self, user_answer: str):
+        weight = int(user_answer)
+        setattr(self, "weight", weight)
 
 
-user_params = UserParams()
+user_params = User()
 
 def start(update: Update, context: CallbackContext):
-    context.bot.send_message(chat_id=update.effective_chat.id, text="Привет! Я бот помогающий тебе освоить разумное питание!\n\nЯ еще очень маленький, и умею только считать нужную тебе для выживания калорийность питания. Введи команду /bmr чтобы я посчитал твой базовый уровень метаболизма.")
+    context.bot.send_message(chat_id=update.effective_chat.id,
+            text=START_MESSAGE) 
 
-def base_metabolic_rate(is_male, age: int, height: int, weight: int):
-    if is_male:
-        r = 66.5 + (13.7 * weight) + (5 * height) - (6.8 * age)
-        return int(r)
+def calculate_female_bmr(user: User) -> int:
+    rate = 655 + (9.6*user.weight) + (1.8*user.height) - (4.7*user.age)
+    return int(rate)
+
+def calculate_male_bmr(user: User) -> int:
+    rate = 66.5 + (13.7*user.weight) + (5*user.height) - (6.8*user.age)
+    return int(rate)
+
+def base_metabolic_rate(user: User) -> int:
+    if user.is_male:
+        return calculate_male_bmr(user)
     else:
-        r = 655 + (9.6 * weight) + (1.8 * height) - (4.7 * age)
-        return int(r)
+        return calculate_female_bmr(user)
 
-def bmr(update: Update, context: CallbackContext):
-    reply_keyboard = [['Мальчик', 'Девочка']]
-    update.message.reply_text(
-        'Давай посчитаем сколько калорий требует твой базовый уровень метаболизма '
-        'Отправь /cancel чтобы прекратить разговор.\n\n'
-        'Ты мальчик или девочка?',
+def bmr(update: Update, context: CallbackContext) -> int:
+    reply_keyboard = [["Мальчик", "Девочка"]]
+    update.message.reply_text(BMR_MESSAGE,        
         reply_markup=ReplyKeyboardMarkup(
-            reply_keyboard, one_time_keyboard=True, input_field_placeholder='мальчик или девочка?'
+            reply_keyboard, 
+            one_time_keyboard=True, 
+            input_field_placeholder = "мальчик или девочка?"
         ),
     )
-
     return GENDER
 
 def gender(update: Update, context: CallbackContext) -> int:
@@ -56,39 +71,34 @@ def gender(update: Update, context: CallbackContext) -> int:
     logger.info("Gender of %s: %s", user.first_name, update.message.text)
     user_params.set_gender(update.message.text)
     update.message.reply_text(
-        'Сколько тебе полных лет?',
+        "Сколько тебе полных лет?",
         reply_markup=ReplyKeyboardRemove(),
     )
-
     return AGE
 
 
 def age(update: Update, context: CallbackContext) -> int:
-    user = update.message.from_user
     user_params.set_age(update.message.text)
     update.message.reply_text(
-        'Сколько ты весишь в килограмммах?',
+        "Сколько ты весишь в килограмммах?",
     )
-
     return WEIGHT
 
 
 def weight(update: Update, context: CallbackContext) -> int:
-    user = update.message.from_user
     user_params.set_weight(update.message.text)
     update.message.reply_text(
-        'Сколько твой рост в сантиметрах?',
+        "Сколько твой рост в сантиметрах?",
     )
 
     return HEIGHT
 
 
 def height(update: Update, context: CallbackContext) -> int:
-    user = update.message.from_user
     user_params.set_height(update.message.text)
 
-    user_bmr = base_metabolic_rate(user_params.is_male, user_params.age, user_params.height, user_params.weight)
-    reply = "Твой расчетный базовый уровень метаболизма равен:\n\n", user_bmr
+    user_bmr = base_metabolic_rate(user_params)
+
     update.message.reply_text(
         f"Твой расчетный базовый уровень метаболизма равен:\n\n {user_bmr}"
     )
@@ -99,9 +109,9 @@ def height(update: Update, context: CallbackContext) -> int:
 
 def cancel(update: Update, context: CallbackContext) -> int:    
     """Cancels and ends the conversation."""
-    user = update.message.from_user
     update.message.reply_text(
-        'Надеюсь мы продолжим позже! Хорошего дня:)', reply_markup=ReplyKeyboardRemove()
+        "Надеюсь мы продолжим позже! Хорошего дня:)",
+        reply_markup=ReplyKeyboardRemove()
     )
 
     return ConversationHandler.END 
